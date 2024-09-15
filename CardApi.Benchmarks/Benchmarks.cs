958ed05ac6e8be199ec36bc2634c68176fa97eac
Michał Toczek <toczekmj@gmail.com>
2024-09-15 11:02:50 +0200

    change tests (#4)
    * update README.md
    
    * update benchmark

--- CardApi.Benchmarks/Benchmarks.cs
+++ CardApi.Benchmarks/Benchmarks.cs
@@ -8,38 +8,33 @@ public class Program
     {
         Console.WriteLine("Press any key to begin benchmarking...");
         Console.ReadKey();
-
-        long[] awaited = new long[5], 
-            whenall = new long[5], 
-            parallel = new long[5], 
-            parallelasync = new long[5];
-
-        Console.WriteLine("\nNaive benchmark for 1000 items");
-        (awaited[0], whenall[0], parallel[0], parallelasync[0]) = await MyFastNaiveBenchmark(1000);
-
-        Console.WriteLine("\nNaive benchmark for 10_000 items");
-        (awaited[1], whenall[1], parallel[1], parallelasync[1]) = await MyFastNaiveBenchmark(10_000);
         
-        Console.WriteLine("\nNaive benchmark for 100_000 items");
-        (awaited[2], whenall[2], parallel[2], parallelasync[2]) = await MyFastNaiveBenchmark(100_000);
-
-        Console.WriteLine("\nNaive benchmark for 1_000_000 items");
-        (awaited[3], whenall[3], parallel[3], parallelasync[3]) = await MyFastNaiveBenchmark(1_000_000);
-
-        Console.WriteLine("\nNaive benchmark for 10_000_000 items");
-        (awaited[4], whenall[4], parallel[4], parallelasync[4]) = await MyFastNaiveBenchmark(10_000_000);
+        var tests = new[]
+        {
+            1000, 2000, 5000, 7000, 10_000, 20_000, 50_000, 70_000, 100_000, 200_000, 500_000, 700_000, 1_000_000,
+            1_500_0000, 2_000_000, 3_000_000, 5_000_000, 7_000_000, 8_000_000, 10_000_000
+        };
+        var amountOfTests = tests.Length;
+        long[] awaited = new long[amountOfTests], whenAll = new long[amountOfTests], 
+               parallel = new long[amountOfTests], parallelAsync = new long[amountOfTests];
+        
+        for(var i = 0; i < amountOfTests; i++)
+        {
+            Console.WriteLine($"\nNaive benchmark for {tests[i]} items");
+            (awaited[i], whenAll[i], parallel[i], parallelAsync[i]) = await MyFastNaiveBenchmark(tests[i]);
+        }
         
         Console.WriteLine("\nNaive benchmark results:");
         Console.WriteLine($"BenchmarkCardsAwaitedAsync: {awaited[0]}ms, {awaited[1]}ms, {awaited[2]}ms, {awaited[3]}ms, {awaited[4]}ms"); 
-        Console.WriteLine($"BenchmarkCardsWhenAllAsync: {whenall[0]}ms, {whenall[1]}ms, {whenall[2]}ms, {whenall[3]}ms, {whenall[4]}ms");
+        Console.WriteLine($"BenchmarkCardsWhenAllAsync: {whenAll[0]}ms, {whenAll[1]}ms, {whenAll[2]}ms, {whenAll[3]}ms, {whenAll[4]}ms");
         Console.WriteLine($"BenchmarkCardsParallel: {parallel[0]}ms, {parallel[1]}ms, {parallel[2]}ms, {parallel[3]}ms, {parallel[4]}ms");
-        Console.WriteLine($"BenchmarkCardsParallelAsync: {parallelasync[0]}ms, {parallelasync[1]}ms, {parallelasync[2]}ms, {parallelasync[3]}ms, {parallelasync[4]}ms");
+        Console.WriteLine($"BenchmarkCardsParallelAsync: {parallelAsync[0]}ms, {parallelAsync[1]}ms, {parallelAsync[2]}ms, {parallelAsync[3]}ms, {parallelAsync[4]}ms");
 
         Console.WriteLine("\nAverage results:");
         Console.WriteLine($"BenchmarkCardsAwaitedAsync: {awaited.Average()}ms");
-        Console.WriteLine($"BenchmarkCardsWhenAllAsync: {whenall.Average()}ms");
+        Console.WriteLine($"BenchmarkCardsWhenAllAsync: {whenAll.Average()}ms");
         Console.WriteLine($"BenchmarkCardsParallel: {parallel.Average()}ms");
-        Console.WriteLine($"BenchmarkCardsParallelAsync: {parallelasync.Average()}ms");
+        Console.WriteLine($"BenchmarkCardsParallelAsync: {parallelAsync.Average()}ms");
         
         
         Console.WriteLine("Press any key to exit...");
